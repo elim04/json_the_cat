3553081a3c9564f662748aa9561059e019385189
Emily Lim <emily.elim04@gmail.com>
2021-01-14 19:22:29 +0000

    refactored breedfetcher to create index.js page, and created test file for unit tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,34 +1,36 @@
 const request = require('request');
 
-//get the command line arg 
-const arg = process.argv.slice(2);
+const fetchBreedDescription = function(breedName, callback) {
+  
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  
+    //edge case for if error in requesting api or URL  it will just console.log the error and exit the function with return
+    if (error) {
+      callback(error, null);
+      return; //error is null if no error
+    }
+  
+    const data = JSON.parse(body);
+  
+    //edge case if breed is not found - data is technically undefined
+    if (data[0]) {
+      callback(null, data[0].description);
+    } else {
+      callback("There is no breed.");
+    }
+  
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${arg[0]}`, (error, response, body) => {
-
-  //edge case for if error in requesting api or URL  it will just console.log the error and exit the function with return
-  if (error) {
-    console.log(error)
-    return //error is null if no error
-  }
-
-  const data = JSON.parse(body);
-
-  //edge case if breed is not found - data is technically undefined
-  if (data[0]) {
-    console.log(data[0].description);
-  } else {
-    console.log("Breed not found.")
-  }
-
-});
+module.exports = { fetchBreedDescription };
 
 
   
-  //after using JSON.parse we have turned data into an object as shown by the console.log
-  // console.log(typeof data);
+//after using JSON.parse we have turned data into an object as shown by the console.log
+// console.log(typeof data);
 
-  //make sure we are getting the body of the search for the siberian cat
-  // console.log('body: ', body);
+//make sure we are getting the body of the search for the siberian cat
+// console.log('body: ', body);
 
-  //before JSON.parse - shows us that the body is a string
-  // console.log(typeof body);
+//before JSON.parse - shows us that the body is a string
+// console.log(typeof body);
